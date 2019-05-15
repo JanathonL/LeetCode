70fd354020cd1eea8d2c56ee6c83db64caf80cee
JanathonL <janathonlljt@gmail.com>
2019-05-15 16:03:54 -0400

    update 247

--- solutions/247. Strobogrammatic Number II.cpp
+++ solutions/247. Strobogrammatic Number II.cpp
@@ -30,3 +30,46 @@ public:
         return res;
     }
 };
+
+// refined time: O(5^n)
+
+class Solution {
+public:
+    vector<string> findStrobogrammatic(int n) {
+        if(n==1){
+            return {"0","1","8"};
+        }
+        unordered_map<char,char> dict;
+        dict['6'] = '9';
+        dict['9'] = '6';
+        dict['8'] = '8';
+        dict['0'] = '0';
+        dict['1'] = '1';
+        vector<string> res;
+        string cur(n, 'x');
+        dfs(res, n, dict, cur, 0);
+        return res;
+    }
+    void dfs(vector<string> &res, int n, unordered_map<char, char> &dict, string cur, int pos){
+        
+        if(pos >= (n+1)/2){
+            res.push_back(cur);
+            return;
+        }
+        for(auto item:dict){
+            if(pos==0 && item.first == '0'){
+                continue;
+            } else if (pos==(n+1)/2-1 && n%2==1 && (item.first=='6' || item.first=='9')) {
+                continue;
+            } else {
+                cur[pos] = item.first;
+                cur[n-pos-1] = item.second;
+                dfs(res, n, dict, cur, pos+1);
+                cur[pos] = 'x';
+                cur[n-pos-1] = 'x';
+            } 
+        }
+    }
+    
+    
+};
